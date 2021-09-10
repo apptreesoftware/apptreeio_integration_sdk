85ffc7a9d5f82a4e074d24ddaba461ddbb04386a
Matthew Smith <matthew.smith@apptreesoftware.com>
2021-09-10 10:46:18 -0700

    Add version support

--- index.ts
+++ index.ts
@@ -6,14 +6,23 @@ import * as express from "express";
 import { createTaskHandler, TaskFunction } from "./lib/task";
 
 const tasks: { [key: string]: TaskFunction } = {};
+let VERSION : string = "VERSION_NOT_SET";
 
 export function addTask(t: TaskFunction, identifier: string) {
   tasks[identifier] = t;
 }
 
+export function setVersion(version: string) {
+  VERSION = version;
+}
+
 export function handleRequest(req: express.Request, resp: express.Response) {
   const components = req.path.split("/");
   const taskId = components[components.length - 1];
+  if (taskId === '_version') {
+    resp.send(`VERSION: ${VERSION}`)
+    return;
+  }
   const taskFunction = tasks[taskId];
   if (!taskFunction) {
     resp.status(404).send(`Task not found: ${req.path}`);
